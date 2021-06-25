9d52325eaf9fec1e44799a694da5d2d8c389eca9
yogi77 <dudipalliyogi@gmail.com>
2021-06-25 12:46:35 +0530

    Update VPCInfraTemplate.groovy

--- vars/VPCInfraTemplate.groovy
+++ vars/VPCInfraTemplate.groovy
@@ -6,30 +6,25 @@ pipeline {
                 git branch: env.BRANCH, credentialsId: '3ee332f3-adb8-4fbf-83ba-4e41e03f5bac', url: env.SCMURL
             }
         }
-    }
-
-    stages {
+   
         stage('Init') {
             steps {
                 bat 'terraform init'
             }
         }
-    }
-    stages {
+    
         stage('Plan') {
             steps {
                 bat 'terraform plan'
             }
         }
-    }
-    stages {
+   
         stage('Apply ') {
             steps {
                 bat 'terraform apply --auto-approve'
             }
         }
-    }
-    stages {
+    
         stage('Destroy ') {
             steps {
                 bat 'terraform destroy --auto-approve'
