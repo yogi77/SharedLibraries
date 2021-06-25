0dbb60e7458221f01a102633ece96118c07af29e
yogi77 <dudipalliyogi@gmail.com>
2021-06-25 16:19:30 +0530

    Create EC2InfraTemplate.groovy

---
+++ vars/EC2InfraTemplate.groovy
@@ -0,0 +1,36 @@
+def call() {
+pipeline {
+    agent any
+    stages {
+        stage('CheckOut') {
+            steps {
+                git branch: env.BRANCH, credentialsId: '3ee332f3-adb8-4fbf-83ba-4e41e03f5bac', url: env.SCMURL
+            }
+        }
+   
+        stage('Init') {
+            steps {
+                bat 'C:\Users\Administrator\Downloads\terraform12\terraform.exe init'
+            }
+        }
+    
+        stage('Plan') {
+            steps {
+                bat 'C:\Users\Administrator\Downloads\terraform12\terraform.exe plan'
+            }
+        }
+   
+        stage('Apply ') {
+            steps {
+                bat 'C:\Users\Administrator\Downloads\terraform12\terraform.exe apply --auto-approve'
+            }
+        }
+    
+        /*stage('Destroy ') {
+            steps {
+                bat 'terraform destroy --auto-approve'
+            }
+        }*/
+    }
+}
+}
